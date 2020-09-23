47fe4863b477c979e92c98ec58beb3215ecb68ad
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-23 09:40:05 -0600

    Create method to test adding recipes and listing those recipes

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -10,4 +10,16 @@ class CookBookTest < Minitest::Test
     assert_instance_of CookBook, cookbook
   end
 
+  def test_recipes_can_be_added_and_listed
+    cookbook = CookBook.new
+
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
+
 end
