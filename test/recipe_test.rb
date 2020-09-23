043e799c1c2c6336ac73f245af8482522bd89ebe
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-23 09:23:51 -0600

    Create Recipe test class

---
+++ test/recipe_test.rb
@@ -0,0 +1,13 @@
+require './test/test_helper'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+
+  def test_it_exists
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_instance_of Recipe, recipe1
+  end
+
+end
